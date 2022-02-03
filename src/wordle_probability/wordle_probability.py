af70bd75b417eaec7369455e8ff33797ccc9a1c5
Matt Miller <mattmiller1@gmail.com>
2022-02-02 19:11:53 -0500

    Add postion counter

--- src/wordle_probability/wordle_probability.py
+++ src/wordle_probability/wordle_probability.py
@@ -8,11 +8,15 @@ class Env:
     """TODO"""
 
     letter_count: Counter = field(default_factory=Counter)
+    position_count: list[Counter] = \
+        field(default_factory=lambda: [Counter() for i in range(5)])
     word_count: int = 0
 
     def get_counts(self, line: str):
         self.word_count += 1
         self.letter_count.update(set(line.rstrip()))
+        for i, c in enumerate(line.rstrip()):
+            self.position_count[i].update(c)
 
     def get_word_metrics(self, line: str):
         pass
