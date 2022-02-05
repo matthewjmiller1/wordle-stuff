321f501f0078073c8958a9184d87a0d36cf59eeb
Matt Miller <mattmiller1@gmail.com>
2022-02-05 14:11:36 -0500

    Add option to prefilter the words

--- src/wordle_probability/wordle_probability.py
+++ src/wordle_probability/wordle_probability.py
@@ -10,6 +10,7 @@ class Env:
 
     args: argparse.Namespace
     orig_word_list: list = field(default_factory=list)
+    word_list: list = field(default_factory=list)
     letter_count: Counter = field(default_factory=Counter)
     position_count: list[Counter] = \
         field(default_factory=lambda: [Counter() for i in range(5)])
@@ -20,16 +21,17 @@ class Env:
         with open(fname) as f:
             for line in f:
                 self.orig_word_list.append(line.rstrip())
-        self.word_count = len(self.orig_word_list)
+        self.word_list = self.orig_word_list
+        self.word_count = len(self.word_list)
 
     def get_counts(self):
-        for word in self.orig_word_list:
+        for word in self.word_list:
             self.letter_count.update(set(word))
             for i, c in enumerate(word):
                 self.position_count[i].update(c)
 
     def get_word_scores(self):
-        for word in self.orig_word_list:
+        for word in self.word_list:
             word_score = 0
             for i, c in enumerate(word):
                 char_score = (
@@ -38,18 +40,28 @@ class Env:
                 word_score += char_score
             self.word_scores[word] = word_score
 
+    def filter_word(self, word: str) -> bool:
+        # Remove words that end in 's' since the words are rarely plural
+        if word.endswith("s"):
+            return True
+
+        # Remove words that have multiple of the same letters since the
+        # words usually don't have multiple of the same letter.
+        if len(word) != len(set(word)):
+            return True
+
+        return False
+
+    def filter_word_list(self):
+        self.word_list = \
+            [w for w in self.orig_word_list if not self.filter_word(w)]
+        self.word_count = len(self.word_list)
+
     def filter_word_scores(self):
         d = {}
         for k, v in self.word_scores.items():
-            # Remove words that end in 's' since the words are rarely plural
-            if k.endswith("s"):
+            if self.filter_word(k):
                 continue
-
-            # Remove words that have multiple of the same letters since the
-            # words usually don't have multiple of the same letter.
-            if len(k) != len(set(k)):
-                continue
-
             d[k] = v
 
         self.word_scores = d
@@ -114,6 +126,10 @@ class Env:
 
 def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--pre-filter', dest='do_prefilter',
+            action='store_true', default=False,
+            help=('filter out plural and multiple letter words before '
+                'computing scores'))
     parser.add_argument('-d', '--debug', dest='do_debug', action='store_true',
             default=False, help='enable debug output')
 
@@ -121,15 +137,25 @@ def main():
 
     env.read_word_list('sgb-words.txt')
 
+    if env.args.do_prefilter:
+        env.filter_word_list()
+
     env.get_counts()
     env.get_word_scores()
 
-    env.filter_word_scores()
+    # If prefilter, the words have already been filtered
+    if not env.args.do_prefilter:
+        env.filter_word_scores()
+
     env.sort_word_scores()
 
-    env.write_letter_probabilities('out/letter_probabilities.txt')
-    env.write_word_scores('out/word_scores.txt')
-    env.write_word_scores_with_second_words('out/word_scores2.txt')
+    suffix = ""
+    if env.args.do_prefilter:
+        suffix = "_prefilter"
+
+    env.write_letter_probabilities(f'out/letter_probabilities{suffix}.txt')
+    env.write_word_scores(f'out/word_scores{suffix}.txt')
+    env.write_word_scores_with_second_words(f'out/word_scores2{suffix}.txt')
 
     if env.args.do_debug:
         print(env)
